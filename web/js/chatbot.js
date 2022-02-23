f01b054e227e627e9dd3489393f058bdba37525f
Faizan Azim <faizanazim11@gmail.com>
2022-02-23 18:59:12 +0530

    GUI chat window baseline done.

---
+++ web/js/chatbot.js
@@ -0,0 +1,9 @@
+function open_chat() {
+    document.getElementById('chat-window').style.visibility='visible';
+    document.getElementById('chat-btn').style.visibility='hidden';
+}
+
+function close_chat() {
+    document.getElementById('chat-window').style.visibility='hidden';
+    document.getElementById('chat-btn').style.visibility='visible';
+}
