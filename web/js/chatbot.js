a60094f578a563fc5594138f751551e05b5ca783
Faizan Azim <faizanazim11@gmail.com>
2022-02-23 21:15:40 +0530

    Chat GUI completed.

--- web/js/chatbot.js
+++ web/js/chatbot.js
@@ -7,3 +7,18 @@ function close_chat() {
     document.getElementById('chat-window').style.visibility='hidden';
     document.getElementById('chat-btn').style.visibility='visible';
 }
+
+function input_data() {
+    input_txt = document.getElementById('msg-input').value;
+    document.getElementById('msg-input').value='';
+    document.getElementById('body-inner').innerHTML+='<span class="msg-right">'+input_txt+'</span>';
+}
+
+var msg_in = document.getElementById('msg-input');
+msg_in.addEventListener("keyup", function(event) {
+    console.log("I was here!!!");
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("msg_btn").click();
+    }
+});
